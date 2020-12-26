ca3f0eccf546c3806d81e658a67ed09e06a72901
Makar41k <sennikovmakar@gmail.com>
2020-12-26 18:19:41 +0300

    second commit

--- app/src/main/java/com/example/makar41k/MainActivity.java
+++ app/src/main/java/com/example/makar41k/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Кирилл. Ты в муте клоун
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
