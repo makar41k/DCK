4c1e4e7cbcdb3fb68379fa4b4ad402f30da9c22c
kropio <60886598+kropio@users.noreply.github.com>
2020-12-26 18:30:27 +0300

    second commit

--- app/src/main/java/com/example/makar41k/MainActivity.java
+++ app/src/main/java/com/example/makar41k/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//паровозик томас неловолен этим проектом (￣ε￣＠)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
